1923860fbfc0cd55b2cd1df14147193edce57dc3
Alijono24 <azamjonnajottalim@gmail.com>
2025-03-10 11:49:39 +0500

    working with webob

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-from wsgiref.simple_server import make_server
+from app import PyFrameApp
 
-def sample_app(environ, start_response):
-    status = "200 OK"
-    hears = [("Content-type", "text/plain")]
-    start_response(status, hears)
-
-    return [b"Assalomu alaykum"]
-
-server = make_server("0.0.0.0", 8000, sample_app)
-server.serve_forever()
+app = PyFrameApp()
